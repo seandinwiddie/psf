e4540a9936da3be49f4fcfedf1dd0569bb0a724e
Sean Dinwiddie <seandinwiddie@gmail.com>
2023-08-28 14:55:38 -0700

    Update spl.js

--- spl.js
+++ spl.js
@@ -1,5 +1,30 @@
 const readline = require('readline');
 
+const store = {
+  state: {
+    progressData: []
+  },
+  reducers: {
+    addProgressData: (state, { rating }) => ({
+      ...state,
+      progressData: [...state.progressData, { rating }]
+    }),
+  },
+  actions: {
+    addProgressData: (rating) => ({
+      type: "ADD_PROGRESS_DATA",
+      payload: { rating }
+    }),
+  },
+  dispatch: (action) => {
+    switch (action.type) {
+      case "ADD_PROGRESS_DATA":
+        store.state = store.reducers.addProgressData(store.state, action.payload);
+        break;
+    }
+  }
+};
+
 function getInput() {
   const rl = readline.createInterface({
     input: process.stdin,
@@ -9,6 +34,7 @@ function getInput() {
   rl.question("Enter progress rating: ", (ratingInput) => {
     const rating = parseFloat(ratingInput);
     rl.close();
+    store.dispatch(store.actions.addProgressData(rating));
     parseInput({ rating });
   });
 }
